6ccdd761a48607c66f57b38f4a18a928543ccf43
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-09-29 12:45:36 -0300

    add models and app

--- copar/copar_app/models.py
+++ copar/copar_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Questionnaire(models.Model):
+    cpf_cnpj = models.CharField(max_length=14, null = False, unique=True)
+    name = models.CharField(max_length=50, null=False)
+    def __str__(self):
+        return self.name
