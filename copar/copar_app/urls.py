069062485784ff194698a4057088af562f03b71b
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-11-24 09:49:56 -0200

    Update urls.py

--- copar/copar_app/urls.py
+++ copar/copar_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.questionnaire, name='questionnaire'),
+    url(r'^$', views.home, name='home'),
+    url(r'^empresarial/new/$', views.questionnaire_emp, name='questionnaire_emp'),
 ]
